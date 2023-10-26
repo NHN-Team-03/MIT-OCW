88b37f497419f94bd5d350f9837b18d4a995bca5
f1v3-dev <f1v3@kakao.com>
2023-10-26 22:17:02 +0900

    Code: chapter1 (Example)

---
+++ seungjo/chapter1/src/Hailstone.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hailstone {
+
+    public static List<Integer> hailstoneSequence(int n) {
+        List<Integer> list = new ArrayList<>();
+        while (n != 1) {
+            list.add(n);
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        }
+        list.add(n);
+        return list;
+    }
+
+    
+    public static void main(String[] args) {
+//        int n = 3;
+//
+//        while (n != 1) {
+//            System.out.println(n);
+//            if (n % 2 == 0) {
+//                n = n / 2;
+//            } else {
+//                n = 3 * n + 1;
+//            }
+//        }
+//        System.out.println(n);
+
+        Hailstone.hailstoneSequence(83)
+                .forEach(System.out::println);
+    }
+}
